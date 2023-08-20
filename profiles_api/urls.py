17fe0c2fb474c9199d90f773ac0a0f018e721059
Sunnat <sunnatbekamirov22@gmail.com>
2023-08-20 16:13:55 +0500

    Added profiles vieset | Profiles vieset qo'shildi.

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,6 +7,7 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register("hello-viewset", views.HelloViewSet, base_name = "hello-viewset")
+router.register("profile", views.UserProfileViewSet)
 
 
 urlpatterns = [
